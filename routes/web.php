fd4467b79d10b62d6c5a2ec8d70a0c1dd5310149
sota <mar.mugi29@gmail.com>
2021-07-05 15:39:23 +0000

    kadai2

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,11 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
-    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
-    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
-    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); 
-    });// 追記
-Route::group(['prefix' => 'admin'], function() {
+    Route::get('XXX', 'AAAController@bbb');
+    Route::get('admin/profile/create', 'Admin\ProfileController@add');
+    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
     Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
     Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
     Route::get('news', 'Admin\NewsController@index')->middleware('auth');
