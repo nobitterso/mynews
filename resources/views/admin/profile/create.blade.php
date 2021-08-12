62294aa91204865d013d0a4f5dac967836548544
sota <mar.mugi29@gmail.com>
2021-08-12 23:50:05 +0000

    kadai6

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,18 +1,10 @@
 
-{{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
 
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', 'ニュースの新規作成')
+@section('title', 'プロフィール')
 
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 mx-auto">
-                <h2>ニュース新規作成</h2>
-                         <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
                         <ul>
@@ -22,61 +14,32 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2">タイトル</label>
+                        <label class="col-md-2" for="title">名前</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2">本文</label>
+                        <label class="col-md-2" for="body">性別</label>
                         <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
+                            <textarea class="form-control" name="gender" rows="20">{{ old('body') }}</textarea>
                         </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">画像</label>
+                        <div class="form-group row">
+                        <label class="col-md-2" for="body">趣味</label>
                         <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
+                            <textarea class="form-control" name="hobby" rows="20">{{ old('body') }}</textarea>
+                        </div><div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('body') }}</textarea>
                         </div>
+                   
                     </div>
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="更新">
                 </form>
-                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
-                     @if (count($errors) > 0)
-                        <ul>
-                            @foreach($errors->all() as $e)
-                                <li>{{ $e }}</li>
-                            @endforeach
-                        </ul>
-                    @endif
-                    <div class="form-group row">
-                        <label class="col-md-2">氏名</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2">性別</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
-                        </div>
-                         <div class="form-group row">
-                        <label class="col-md-2">趣味</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
-                        </div>
-                         <div class="form-group row">
-                        <label class="col-md-2">自己紹介欄</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
-                        </div>
-
-
-
-                    {{ csrf_field() }}
-                    <input type="submit" class="btn btn-primary" value="更新">
             </div>
         </div>
     </div>
+  
 @endsection
